6cf1d324c3550c5b185e5e71ce507f582a3d51c8
tsato1 <tsato1@ucsc.edu>
2015-10-16 15:58:12 -0700

    jQuery mosaic for fade explanation done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function ($) {
+    /*$(".figure").hover(function () {
+        $(this).css("opacity", "0.4")
+    }, function() {
+        $(this).css("opacity", "1.0")
+    });*/
+    $('.figure').mosaic({
+        animation: 'fade'
+    });
+    
+});
