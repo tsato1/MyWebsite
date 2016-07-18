832934e5c5cd62aafa836c0fc4b17d1069646bb6
tsato1 <a8tb1117@gmail.com>
2016-07-17 17:18:54 -0700

    feat: new web design project added. Mosaic overlay is fixed so that it will toggle when cursor is on the object.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function ($) {
-    /*$(".figure").hover(function () {
-        $(this).css("opacity", "0.4")
-    }, function() {
-        $(this).css("opacity", "1.0")
-    });*/
+    $(".mosaic-overlay").hover(
+    	function () {
+    		$(this).css("background-color", "#111")
+    	}, 
+        function () {
+        	$(this).css("color", "#fff")
+        },
+        function () {
+        	$(".details").details.css("display", "block")
+        }
+    );
     $('.figure').mosaic({
         animation: 'fade'
     });
-    
 });
